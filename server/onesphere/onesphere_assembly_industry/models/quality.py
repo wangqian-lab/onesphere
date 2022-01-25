5bb1494a1e1b217493798abc2f588f1348f2bd52
centron <2740335954@qq.com>
2022-01-25 11:24:51 +0800

    [ADD] (onesphere_assembly_industry) 增加工步版本字段。

--- server/onesphere/onesphere_assembly_industry/models/quality.py
+++ server/onesphere/onesphere_assembly_industry/models/quality.py
@@ -9,6 +9,8 @@ class OneshareQuality(models.Model):
 
     tightening_opr_point_ids = fields.One2many('onesphere.tightening.opr.point', 'parent_quality_point_id')
 
+    step_version = fields.Integer(default=1)
+
     def get_tightening_operation_points(self, *args, **kwargs):
         ret = []
         self.ensure_one()
@@ -17,3 +19,11 @@ class OneshareQuality(models.Model):
         for point in self.tightening_opr_point_ids:
             ret.append({'y_offset': point.y_offset, 'x_offset': point.x_offset})
         return ret
+
+    def write(self, vals):
+        ver = self.step_version
+        vals.update({"step_version": ver + 1})
+        # 修改使用了该工步的作业的版本号
+        for data in self.onesphere_operation_ids:
+            data.operation_id.oper_version += 1
+        return super(OneshareQuality, self).write(vals)

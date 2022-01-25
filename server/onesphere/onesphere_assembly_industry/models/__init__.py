c8fff97e1651c1149f923b293d08c491024532bf
centron <2740335954@qq.com>
2022-01-25 16:23:46 +0800

    [ADD] (onesphere_assembly_industry) 增加作业模型和视图的加载。

--- server/onesphere/onesphere_assembly_industry/models/__init__.py
+++ server/onesphere/onesphere_assembly_industry/models/__init__.py
@@ -6,6 +6,6 @@ from . import mrp_workcenter_group_tool
 from . import product
 from . import tightening_opr_point
 from . import tightening_result
-# from . import mrp_routing_workcenter
+from . import mrp_routing_workcenter
 from . import real_oper_version
 from . import mrp_bom

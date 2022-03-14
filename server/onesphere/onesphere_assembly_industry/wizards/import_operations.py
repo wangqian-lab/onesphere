85c009e4fe497fd6462ead5c943d3bc3c070e516
centron <2740335954@qq.com>
2022-03-14 11:40:28 +0800

    [FIX](onesphere_assembly_industry): 修改作业导入功能的工具改为拧紧单元。

--- server/onesphere/onesphere_assembly_industry/wizards/import_operations.py
+++ server/onesphere/onesphere_assembly_industry/wizards/import_operations.py
@@ -15,16 +15,18 @@ _logger = logging.getLogger(__name__)
 FIRST_DATA_ROW = 3
 
 COLUMN_STEP_TYPE = 5
+COLUMN_OPERATION_NAME = 0
 COLUMN_OPERATION_CODE = 1
 COLUMN_WORKCENTER_CODE = 2
 COLUMN_STEP_CODE = 4
 COLUMN_PRODUCT_CODE = 6
 COLUMN_STEP_NOTE = 7
-COLUMN_TOOL_SN_STR = 9
+COLUMN_TIGHTENING_UNIT_STR = 9
 COLUMN_SCREW_CODE = 11
 COLUMN_TIGHTENING_PSET = 12
 COLUMN_TiGHTENING_IMG = 8
 COLUMN_TIGHTENING_POINT_NAME = 10
+
 
 class ImportOperation(models.TransientModel):
     _name = 'onesphere.import.operation'
@@ -33,12 +35,17 @@ class ImportOperation(models.TransientModel):
 
     def _create_operation(self, data):
         operation_code = data.cell_value(FIRST_DATA_ROW, COLUMN_OPERATION_CODE)
+        operation_name = data.cell_value(FIRST_DATA_ROW, COLUMN_OPERATION_NAME)
         workcenter_code = data.cell_value(FIRST_DATA_ROW, COLUMN_WORKCENTER_CODE)
         workcenter = self.env['mrp.workcenter'].search([('code', '=', workcenter_code)])
         if not workcenter:
             raise ValidationError(f'Can Not Found Workcenter,Code{workcenter_code}')
+        exist_operation = self.env['mrp.routing.workcenter'].search([('code', '=', operation_code)])
+        if exist_operation:
+            raise ValidationError(f'Already Exist Operation,Code:{operation_code}')
         operation_data = {
-            'name': operation_code,
+            'name': operation_name,
+            'code': operation_code,
             'workcenter_id': workcenter.id,
         }
         operation = self.env['mrp.routing.workcenter'].create(operation_data)
@@ -66,12 +73,18 @@ class ImportOperation(models.TransientModel):
             'test_type_id': test_type_id,
             'component_id': product_id,
             'note': step_note,
+            'is_workorder_step': True,
         }
         if step_type in ALL_TIGHTENING_TEST_TYPE_LIST and tightening_img:
             img_url = self.img_url + '\\' + tightening_img
             with open(img_url, "rb") as f:
                 img_bin = base64.b64encode(f.read())
             step_dic.update({'worksheet_img': img_bin})
+
+        operation_type = self.env['oneshare.operation.type'].search([('code', '=', 'mrp_operation')])
+        if operation_type:
+            step_dic.update({'operation_type_ids': [(4, operation_type.id)]})
+
         step = self.env['oneshare.quality.point'].create(step_dic)
 
         operation_step_rel_dic = {
@@ -85,19 +98,27 @@ class ImportOperation(models.TransientModel):
     def _create_tightening_point(self, step_data, step, tightening_points_seq):
         if not step:
             raise ValidationError('No step to add tightening points!')
-        tool_group_ids = []
-        tool_sn_str = ustr(step_data[COLUMN_TOOL_SN_STR])
+        tightening_unit_ids = []
+        tightening_unit_str = ustr(step_data[COLUMN_TIGHTENING_UNIT_STR])
         screw_code = step_data[COLUMN_SCREW_CODE]
         tightening_pset = step_data[COLUMN_TIGHTENING_PSET]
         tightening_point_name = step_data[COLUMN_TIGHTENING_POINT_NAME]
-        tool_sn_list = tool_sn_str.split(',')
-        for tool_sn in tool_sn_list:
-            tool = self.env['maintenance.equipment'].search([('serial_no', '=', tool_sn)])
-            if not tool:
-                raise ValidationError(f'Can not found tool,serial_no:{tool_sn}')
-            tool_group_id = self.env['mrp.workcenter.group.tightening.tool'].search(
-                [('tightening_tool_id', '=', tool.id)]).id
-            tool_group_ids.append(tool_group_id)
+        tightening_unit_list = tightening_unit_str.split(',')
+        for tightening_unit in tightening_unit_list:
+            controller_sn, unit_code = tightening_unit.split('-')[0], tightening_unit.split('-')[1]
+            controller = self.env['maintenance.equipment'].search([('serial_no', '=', controller_sn)])
+            if not controller:
+                raise ValidationError(f'Can not found tool,serial_no:{controller_sn}')
+            # tool_group_id = self.env['mrp.workcenter.group.tightening.tool'].search(
+            #     [('tightening_tool_id', '=', tool.id)]).id
+            # tool_group_ids.append(tool_group_id)
+            tightening_unit = self.env['onesphere.tightening.unit'].search([('tightening_tool_id', '=', controller.id),
+                                                                            ('ref', '=', unit_code)])
+            # if not tightening_unit:
+            #     raise ValidationError(f'Can not found tightening_unit,serial_no:{controller_sn},unit_code:{unit_code}!')
+            if not tightening_unit:
+                continue
+            tightening_unit_ids.append(tightening_unit.id)
 
         screw_model = self.env['product.product']
         screw = screw_model.search([('default_code', '=', screw_code)])
@@ -111,7 +132,7 @@ class ImportOperation(models.TransientModel):
 
         point_dic = {
             'name': tightening_point_name,
-            'tightening_tool_ids': [(6, 0, tool_group_ids)],
+            'tightening_units': [(6, 0, tightening_unit_ids)],
             'product_id': screw.id,
             'tightening_pet': tightening_pset,
             'parent_quality_point_id': step.id,
@@ -157,4 +178,4 @@ class ImportOperation(models.TransientModel):
                 self.env.user.notify_success(f'Create Operation Success,Operation Code:{operation_code}')
             except Exception as e:
                 _logger.error(f'Create Operation Failed,Reason:{ustr(e)}')
-                self.env.user.notify_warning(f'Create Operation Failed,Operation Code:{operation_code}')
+                self.env.user.notify_warning(f'Create Operation Failed,Operation Code:{operation_code},reason:{ustr(e)}')

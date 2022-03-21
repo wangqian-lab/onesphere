867545b11017d5f333f4740b1a9a22a8d8ce3609
centron <2740335954@qq.com>
2022-03-21 11:36:30 +0800

    [FIX](onesphere_assembly_industry): 去掉多余的包引用。

--- server/onesphere/onesphere_assembly_industry/wizards/assembly_industry_spc.py
+++ server/onesphere/onesphere_assembly_industry/wizards/assembly_industry_spc.py
@@ -3,8 +3,6 @@ from odoo import api, exceptions, fields, models, _
 import logging
 from odoo.exceptions import ValidationError
 from odoo.addons.oneshare_utils.constants import ONESHARE_DEFAULT_SPC_MIN_LIMIT, ONESHARE_DEFAULT_SPC_MAX_LIMIT
-from pandas import DataFrame
-import pprint
 import numpy as np
 from odoo.addons.lexen_spc.plot import normal, histogram
 from odoo.addons.lexen_spc.chart import cmk, cpk, xbar_rbar, rbar, covert2dArray

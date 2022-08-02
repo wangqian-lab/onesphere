a90287e080e0a64f3fea9f47218cbb7ba79115cb
centron <2740335954@qq.com>
2022-08-02 16:24:04 +0800

    [FIX](onesphere_assembly_industry) 修改拧紧圆点的大小，修复第一次移动点位再打开会错位的bug。 (onesphere_mdm) 将下发所有类型工步选项默认设为True。 (oneshare_cap_assembly) form视图增加必填字段，修改代码。

--- server/onesphere/onesphere_mdm/models/res_config_settings.py
+++ server/onesphere/onesphere_mdm/models/res_config_settings.py
@@ -5,6 +5,6 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    send_operation_all_step = fields.Boolean(default=False, string='下发工艺所有工步',
+    send_operation_all_step = fields.Boolean(default=True, string='下发工艺所有工步',
                                              help='Send All Steps of Operation To Make Sure Work Center Can Make Orders By Scan',
                                              config_parameter='oneshare.send.all.steps')

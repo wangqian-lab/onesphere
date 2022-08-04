a7c63454ef89dffbf5c8aeac03536f81546cfb91
masami10 <masami.nagasawa10@hotmail.com>
2022-08-04 14:55:36 +0800

    [FEAT](wave)增加ENV_BACKUP_WITH_MINIO环境变量是否可以备份minio数据

--- server/onesphere/onesphere_wave/controllers/main.py
+++ server/onesphere/onesphere_wave/controllers/main.py
@@ -7,11 +7,14 @@ import zipfile
 
 import docker
 import werkzeug
+from distutils.util import strtobool
 from odoo.addons.web.controllers.main import Database as webDatabaseController
 
 from odoo import http
 
 ENV_DOCKER_URL = os.getenv('ENV_DOCKER_URL', 'unix://var/run/docker.sock')
+
+ENV_BACKUP_WITH_MINIO = strtobool(os.getenv('ENV_BACKUP_WITH_MINIO', 'False'))
 
 client = docker.DockerClient(base_url=ENV_DOCKER_URL)
 
@@ -45,14 +48,16 @@ class Database(webDatabaseController):
 
     @http.route()
     def backup(self, master_pwd, name, backup_format='zip'):
-        request = http.request
-        platform = request.user_agent.platform
-        containers = client.containers.list(filters={'status': 'running'})
+        # request = http.request
+        # platform = request.httprequest.user_agent.platform
         resp = super(Database, self).backup(master_pwd, name, backup_format)
         if not resp.is_streamed:
             return resp
+        if not ENV_BACKUP_WITH_MINIO:
+            return resp
         stream = resp.response
         minio_container = None
+        containers = client.containers.list(filters={'status': 'running'})
         for container in containers:
             image_name = container.image.tags[0]
             if 'minio' in image_name:

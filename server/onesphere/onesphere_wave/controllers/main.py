896f8f4665630b66b0802c6a1a8c6fc095fe5491
masami10 <masami.nagasawa10@hotmail.com>
2022-08-05 08:30:40 +0800

    [FEAT](wave)备份minio数据

--- server/onesphere/onesphere_wave/controllers/main.py
+++ server/onesphere/onesphere_wave/controllers/main.py
@@ -3,30 +3,74 @@
 import logging
 import os
 import tempfile
+import zipfile
 from distutils.util import strtobool
 
 import docker
 import werkzeug
+import werkzeug.exceptions
+import werkzeug.utils
+import werkzeug.wrappers
+import werkzeug.wsgi
 from odoo.addons.oneshare_utils.zip import zip_dir
 from odoo.addons.web.controllers.main import Database as webDatabaseController
 
+import odoo
+import odoo.modules.registry
+import odoo.release
+import odoo.sql_db
 from odoo import http
+from odoo.http import dispatch_rpc
+from odoo.service import db
+from odoo.tools import ustr
+from odoo.tools.misc import str2bool
 
 ENV_DOCKER_URL = os.getenv('ENV_DOCKER_URL', 'unix://var/run/docker.sock')
 
 ENV_BACKUP_WITH_MINIO = strtobool(os.getenv('ENV_BACKUP_WITH_MINIO', 'False'))
-
-client = docker.DockerClient(base_url=ENV_DOCKER_URL)
-
 _logger = logging.getLogger(__name__)
+
+try:
+    client = docker.DockerClient(base_url=ENV_DOCKER_URL)
+except Exception as e:
+    _logger.error(ustr(e))
+    client = None
+
+CONTAINER_STOP_TIMEOUT = 20
+
+
+def restore_minio_data(zip_fn):
+    minio_container = None
+    if not client:
+        return
+    containers = client.containers.list()
+    for container in containers:
+        image_name = container.image.tags[0]
+        if 'minio' in image_name:
+            minio_container = container
+            break
+    if not minio_container or not ENV_BACKUP_WITH_MINIO:
+        return
+    with tempfile.TemporaryDirectory() as dump_dir:
+        if zipfile.is_zipfile(zip_fn):
+            # v8 format
+            with zipfile.ZipFile(zip_fn, 'r') as z:
+                z.extract('backup.tar', dump_dir)
+                # 需要关闭重启
+                minio_container.stop(timeout=CONTAINER_STOP_TIMEOUT)  # 先关闭容器
+                c = client.containers.run('ubuntu', volumes_from=[minio_container.name], detach=True,
+                                          volumes=[f'{dump_dir}:/backup'],
+                                          command='''bash -c "cd /data && tar xvf /backup/backup.tar --strip 1" ''')
+                exit_status = c.wait()['StatusCode']
+                _logger.info(f'容器返回值: {exit_status}')
+                c.remove()
+                minio_container.start()
 
 
 class Database(webDatabaseController):
 
     @http.route()
     def backup(self, master_pwd, name, backup_format='zip'):
-        # request = http.request
-        # platform = request.httprequest.user_agent.platform
         resp = super(Database, self).backup(master_pwd, name, backup_format)
         if not resp.is_streamed:
             return resp
@@ -34,6 +78,8 @@ class Database(webDatabaseController):
             return resp
         stream = resp.response
         minio_container = None
+        if not client:
+            return resp
         containers = client.containers.list(filters={'status': 'running'})
         for container in containers:
             image_name = container.image.tags[0]
@@ -45,12 +91,14 @@ class Database(webDatabaseController):
             return resp
         with tempfile.TemporaryDirectory() as tmp_dir:
             try:
+                minio_container.stop(timeout=CONTAINER_STOP_TIMEOUT)  # 先关闭容器
                 c = client.containers.run('ubuntu', volumes_from=[minio_container.name], detach=True,
                                           volumes=[f'{tmp_dir}:/backup'],
                                           command='tar cvf /backup/backup.tar /data')
                 exit_status = c.wait()['StatusCode']
                 _logger.info(f'容器返回值: {exit_status}')
                 c.remove()
+                minio_container.start()
             except Exception as e:
                 pass
             zip_dir(tmp_dir, stream, mode='a', include_dir=False,
@@ -58,3 +106,24 @@ class Database(webDatabaseController):
             stream.seek(0)
         response = werkzeug.wrappers.Response(stream, headers=resp.headers, direct_passthrough=True)
         return response
+
+    @http.route()
+    def restore(self, master_pwd, backup_file, name, copy=False):
+        insecure = odoo.tools.config.verify_admin_password('admin')
+        if insecure and master_pwd:
+            dispatch_rpc('db', 'change_admin_password', ["admin", master_pwd])
+        try:
+            data_file = None
+            db.check_super(master_pwd)
+            with tempfile.NamedTemporaryFile(delete=False) as data_file:
+                backup_file.save(data_file)
+            db.restore_db(name, data_file.name, str2bool(copy))
+            if ENV_BACKUP_WITH_MINIO:
+                restore_minio_data(data_file.name)
+            return http.local_redirect('/web/database/manager')
+        except Exception as e:
+            error = "Database restore error: %s" % (str(e) or repr(e))
+            return self._render_template(error=error)
+        finally:
+            if data_file:
+                os.unlink(data_file.name)

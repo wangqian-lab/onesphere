dbb7446a3bf75689088c06af201a584c0762d4db
masami10 <masami.nagasawa10@hotmail.com>
2022-09-15 17:16:37 +0800

    [FIX]修复了数据问题

--- script/gen_demo_tightening_cap_log.py
+++ script/gen_demo_tightening_cap_log.py
@@ -32,14 +32,9 @@ G_TMPL = Template('''
 ''')
 
 RECORD_TMPL = Template('''
-        <record id="cap_tightening_log_{{ id }}" model="oneshare.cap.tightening.analysis.log">
-            <field name="tightening_result_entity_id">{{ tightening_result_entity_id }}</field>
-            <field name="tightening_track_code">{{ tightening_track_code }}</field>
-            <field name="tightening_tool_entity_id">{{ tightening_tool_entity_id }}</field>
+        <record id="onesphere_assembly_industry.tightening_result_{{ id }}"  model="onesphere.tightening.result">
             <field name="analysis_result_state">{{ analysis_result_state }}</field>
-            <field name="tightening_bolt_id" ref="{{ tightening_bolt_id }}"/>
             <field name="cap_error_massage">{{ cap_error_massage }}</field>
-            <field name="time" eval="(DateTime.today() - relativedelta(days={{ delta_day }})).strftime('%Y-%m-%d %H:%M')"/>
         </record>
 ''')
 
@@ -66,27 +61,19 @@ if __name__ == '__main__':
         os.remove(DIST_PATH)
     rec_str = []
     result_dict = {}
-    with open(DIST_RESULT_PATH) as f:
-        a = f.read()
-        result_dict = xmltodict.parse(a)
-    results = result_dict.get('odoo', {}).get('data', {}).get('record', [])
-    for i, r in enumerate(results):
+    # with open(DIST_RESULT_PATH) as f:
+    #     a = f.read()
+    #     result_dict = xmltodict.parse(a)
+    # results = result_dict.get('odoo', {}).get('data', {}).get('record', [])
+    for i in range(1000):
         delta_day = random.choice([1, 2])
-        d = convert_fields_to_dict(r.get('field'))
-        tightening_result_entity_id = d.get('entity_id').get('#text')
-        tightening_track_code = d.get('track_no').get('#text')
-        tightening_tool_entity_id = d.get('attribute_equipment_no').get('#text')
-        tightening_bolt_id = d.get('tightening_point_name').get('@ref')
         analysis_result_state = random.choice(['ok', 'nok'])
         cap_error_massage = random.choice(['', '101', '101,102', '101,102,103'])
+        if analysis_result_state == 'ok':
+            cap_error_massage = ''
         m = gen_record_msg(id=i,
-                           tightening_result_entity_id=tightening_result_entity_id,
-                           tightening_tool_entity_id=tightening_tool_entity_id,
-                           tightening_bolt_id=tightening_bolt_id,
                            analysis_result_state=analysis_result_state,
-                           delta_day=delta_day,
-                           cap_error_massage=cap_error_massage,
-                           tightening_track_code=tightening_track_code)
+                           cap_error_massage=cap_error_massage)
         rec_str.append(m)
     ss = G_TMPL.render(items=rec_str)
     with open(DIST_PATH, 'w') as f:

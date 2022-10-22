1f1fde3b4417f0c1949359f1cc1366377dd35610
masami10 <masami.nagasawa10@hotmail.com>
2022-10-22 13:03:16 +0800

    [IMP]完善是否需要自动创建attachment的bucket的功能

--- server/onesphere/ir_attachment_s3/__init__.py
+++ server/onesphere/ir_attachment_s3/__init__.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
 
 from odoo import api, SUPERUSER_ID
 from odoo.tools import ustr
+from odoo.tools.misc import str2bool
 from . import models
 from . import tests
 
 _logger = logging.getLogger(__name__)
 
+ENV_FORCE_CREATE_ATTACH_BUCKET = str2bool(os.getenv('ENV_FORCE_CREATE_ATTACH_BUCKET', 'False'))
+
 
 def _ir_attachment_s3_post_init(cr, registry):
+    if not ENV_FORCE_CREATE_ATTACH_BUCKET:
+        return
     env = api.Environment(cr, SUPERUSER_ID, {})
     ICP = env['ir.config_parameter']
     oss_interface = env['onesphere.oss.interface']

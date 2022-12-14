1a397b53b7043b53faad9e1a64c9bd3c37adbdb6
masami10 <masami.nagasawa10@hotmail.com>
2022-12-14 21:12:58 +0800

    [FIX](mdm)修复了获取默认路线错误

--- server/onesphere/onesphere_mdm/models/product.py
+++ server/onesphere/onesphere_mdm/models/product.py
@@ -7,9 +7,10 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
     _check_company_auto = True
 
-    # 目前只能想到这种方法，action中env ref，obj等字段不支持
     @api.model
     def default_select_ids(self):
-        item = self.env['ir.model.data'].xmlid_to_res_id('mrp.route_warehouse0_manufacture')
-        return [[6, 0, [item]]]
-    route_ids = fields.Many2many(default=default_select_ids)
+        item = self.env.ref('mrp.route_warehouse0_manufacture', raise_if_not_found=False)
+        if item:
+            return item.ids
+        return []
+    route_ids = fields.Many2many(default=lambda self: self.default_select_ids())

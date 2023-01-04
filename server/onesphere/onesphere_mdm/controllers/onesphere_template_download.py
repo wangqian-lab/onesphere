4100210803ae70022f1fb5f245af244370f78e19
wangqian-lab <2740335954@qq.com>
2023-01-04 13:37:43 +0800

    [FIX] (onesphere_mdm, assembly_industry) 作业模板下载功能挪到作业导入中。

--- server/onesphere/onesphere_mdm/controllers/onesphere_template_download.py
+++ server/onesphere/onesphere_mdm/controllers/onesphere_template_download.py
@@ -8,57 +8,72 @@ import logging
 import binascii
 import zipfile
 import mimetypes
+import os
 
 _logger = logging.getLogger(__name__)
 
 
 class OneshareTemplateDownloadController(Controller):
 
-    def _get_attachment_type(self, template_record):
-        attachment_record = request.env['ir.attachment'].sudo().search(
-            [('res_model', '=', 'onesphere.template.download'),
-             ('res_field', '=', 'template_file'),  # 必须带res_field字段
-             ('res_id', 'in', template_record.ids)])
-        if not attachment_record:
-            raise ValidationError(_(f'Can not find attachment! record id is {template_record.id}'))
-        mimetype = attachment_record.mimetype
-        file_extension = mimetypes.guess_extension(mimetype)
-        return mimetype, file_extension
-
-    def multi_template_download(self, temp_file, template_records):
-        with zipfile.ZipFile(temp_file, 'w', compression=zipfile.ZIP_DEFLATED) as zfp:
-            for template_record in template_records:
-                template_obj = template_record.template_file
-                mimetype, file_extension = self._get_attachment_type(template_record)
-                file_name = f'{template_record.name}{file_extension}'
-                zfp.writestr(file_name, binascii.a2b_base64(template_obj))
-        res = send_file(temp_file, mimetype="application/zip", filename='templates.zip', as_attachment=True)
-        return res
-
-    def single_template_download(self, temp_file, template_record):
-        template_obj = template_record.template_file
-        temp_file.write(binascii.a2b_base64(template_obj))
-        mimetype, file_extension = self._get_attachment_type(template_record)
-        file_name = f'{template_record.name}{file_extension}'.encode('utf-8')
-        res = send_file(temp_file, mimetype=mimetype, filename=file_name, as_attachment=True)
-        return res
+    # def _get_attachment_type(self, template_record):
+    #     attachment_record = request.env['ir.attachment'].sudo().search(
+    #         [('res_model', '=', 'onesphere.template.download'),
+    #          ('res_field', '=', 'template_file'),  # 必须带res_field字段
+    #          ('res_id', 'in', template_record.ids)])
+    #     if not attachment_record:
+    #         raise ValidationError(_(f'Can not find attachment! record id is {template_record.id}'))
+    #     mimetype = attachment_record.mimetype
+    #     file_extension = mimetypes.guess_extension(mimetype)
+    #     return mimetype, file_extension
+    #
+    # def multi_template_download(self, temp_file, template_records):
+    #     with zipfile.ZipFile(temp_file, 'w', compression=zipfile.ZIP_DEFLATED) as zfp:
+    #         for template_record in template_records:
+    #             template_obj = template_record.template_file
+    #             mimetype, file_extension = self._get_attachment_type(template_record)
+    #             file_name = f'{template_record.name}{file_extension}'
+    #             zfp.writestr(file_name, binascii.a2b_base64(template_obj))
+    #     res = send_file(temp_file, mimetype="application/zip", filename='templates.zip', as_attachment=True)
+    #     return res
+    #
+    # def single_template_download(self, temp_file, template_record):
+    #     template_obj = template_record.template_file
+    #     temp_file.write(binascii.a2b_base64(template_obj))
+    #     mimetype, file_extension = self._get_attachment_type(template_record)
+    #     file_name = f'{template_record.name}{file_extension}'.encode('utf-8')
+    #     res = send_file(temp_file, mimetype=mimetype, filename=file_name, as_attachment=True)
+    #     return res
+    #
+    # @http.route('/oneshare/template_download', type='http', auth='user', cors='*',
+    #             csrf=False,
+    #             save_session=False)
+    # def template_download(self):
+    #     """
+    #     Downloads the template excel
+    #     """
+    #     record_ids_list = request.params.get('ids', '').split(',')
+    #     record_ids = [int(id) for id in record_ids_list]
+    #     template_records = request.env['onesphere.template.download'].search([('id', 'in', record_ids)])
+    #     if not template_records:
+    #         raise ValidationError(_('No template record!'))
+    #     temp_file = tempfile.TemporaryFile()
+    #     if len(template_records) > 1:
+    #         res = self.multi_template_download(temp_file, template_records)
+    #     else:
+    #         res = self.single_template_download(temp_file, template_records)
+    #     res.headers['Cache-Control'] = 'no-cache'
+    #     return res
 
     @http.route('/oneshare/template_download', type='http', auth='user', cors='*',
                 csrf=False,
                 save_session=False)
     def template_download(self):
         """
-        Downloads the template excel
+        Downloads the template by path
         """
-        record_ids_list = request.params.get('ids', '').split(',')
-        record_ids = [int(id) for id in record_ids_list]
-        template_records = request.env['onesphere.template.download'].search([('id', 'in', record_ids)])
-        if not template_records:
-            raise ValidationError(_('No template record!'))
-        temp_file = tempfile.TemporaryFile()
-        if len(template_records) > 1:
-            res = self.multi_template_download(temp_file, template_records)
-        else:
-            res = self.single_template_download(temp_file, template_records)
+        template_path = request.params.get('template_path', '')
+        filename = os.path.basename(template_path)
+        mimetype = mimetypes.guess_type(filename)
+        res = send_file(template_path, mimetype=mimetype[0], filename=filename.encode('utf-8'), as_attachment=True)
         res.headers['Cache-Control'] = 'no-cache'
         return res
